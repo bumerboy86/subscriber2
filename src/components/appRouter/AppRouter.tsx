f42aa5e0cce2a12ada0721ea84f7b62a262993cf
ruslan <bumerboy86@gmail.com>
2024-02-27 16:53:04 +0600

    test

--- src/components/appRouter/AppRouter.tsx
+++ src/components/appRouter/AppRouter.tsx
@@ -1,19 +1,23 @@
-import { FC } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { FC } from "react";
+import { Route, Routes } from "react-router-dom";
 
-import AppWrapper from '../../layout/AppWrapper'
-import { publicRoutes } from '../../router/routes'
+import AppWrapper from "../../layout/AppWrapper";
+import { publicRoutes } from "../../router/routes";
 
 const AppRouter: FC = () => {
   return (
     <Routes>
-      <Route path='/' element={<AppWrapper />}>
-        {publicRoutes.map(route => (
-          <Route path={route.path} element={<route.component />} key={route.path} />
+      <Route path='/subscriber2' element={<AppWrapper />}>
+        {publicRoutes.map((route) => (
+          <Route
+            path={route.path}
+            element={<route.component />}
+            key={route.path}
+          />
         ))}
       </Route>
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
